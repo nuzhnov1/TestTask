f87190024f40b65bffc596a7027cb23fadc9064a
sunman24 <andrey_nuzhnov.01@mail.ru>
2023-06-28 16:10:37 +0700

    Implemented the MainActivity class and layout of the car card

---
+++ app/src/main/kotlin/com/nuzhnov/testtask/presentation/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.nuzhnov.testtask.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+}
