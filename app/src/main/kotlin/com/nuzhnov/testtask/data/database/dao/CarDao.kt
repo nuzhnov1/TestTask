b794d2c25b527773c1c282990c5ac61e5c473acf
sunman24 <andrey_nuzhnov.01@mail.ru>
2023-06-30 09:07:26 +0700

    Updated data extraction from the database

--- app/src/main/kotlin/com/nuzhnov/testtask/data/database/dao/CarDao.kt
+++ app/src/main/kotlin/com/nuzhnov/testtask/data/database/dao/CarDao.kt
@@ -3,12 +3,36 @@ package com.nuzhnov.testtask.data.database.dao
 import com.nuzhnov.testtask.data.database.entity.CarEntity
 import com.nuzhnov.testtask.data.database.entity.CarEntity.Companion.NUMBER_FIELD_NAME
 import com.nuzhnov.testtask.data.database.entity.CarEntity.Companion.TABLE_NAME
+import com.nuzhnov.testtask.data.mapper.toQueryString
+import com.nuzhnov.testtask.domen.model.CarSortType
+import com.nuzhnov.testtask.domen.model.SortOrder
 import kotlinx.coroutines.flow.Flow
 import androidx.room.Dao
-import androidx.room.Query
+import androidx.room.RawQuery
+import androidx.sqlite.db.SimpleSQLiteQuery
+import androidx.sqlite.db.SupportSQLiteQuery
 
 @Dao
-internal interface CarDao {
-    @Query("SELECT * FROM $TABLE_NAME WHERE $NUMBER_FIELD_NAME LIKE :numberPattern")
-    fun getCarEntitiesFlow(numberPattern: String): Flow<List<CarEntity>>
+internal abstract class CarDao {
+    fun getCarEntitiesFlow(
+        number: String,
+        sortType: CarSortType,
+        sortOrder: SortOrder
+    ): Flow<List<CarEntity>> {
+        val numberPattern = "%$number%"
+        val sortTypeString = sortType.toQueryString()
+        val sortOrderString = sortOrder.toQueryString()
+
+        val query = SimpleSQLiteQuery(
+            """
+            SELECT * FROM $TABLE_NAME WHERE $NUMBER_FIELD_NAME LIKE '$numberPattern'
+            ORDER BY $sortTypeString $sortOrderString
+            """.trimIndent().replace(oldChar = '\n', newChar = ' ')
+        )
+
+        return getCarEntitiesFlow(query)
+    }
+
+    @RawQuery(observedEntities = [CarEntity::class])
+    protected abstract fun getCarEntitiesFlow(query: SupportSQLiteQuery): Flow<List<CarEntity>>
 }
